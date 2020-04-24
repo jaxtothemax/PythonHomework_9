bcbf2e47faded380be52f1277d9c567d054d6a1b
jaxtothemax <jax.atlet@hotmail.com>
2020-04-24 23:05:19 +0200

    added string interpolation bonus homework

---
+++ String_Interpolation.py
@@ -0,0 +1,5 @@
+username = input("Enter your name: ")
+age = input("Enter your age: ")
+home = input("Enter where you live: ")
+
+print(f"My name is {username} and I am {age} years old. I live in {home}.")
