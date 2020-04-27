1f3707e332071a21ef66d4643b2618a512104ac3
jaxtothemax <jax.atlet@hotmail.com>
2020-04-27 21:12:28 +0200

    fixed the conditions and loops in fizzbuzz, added a constant in mi to km / km to mi conversion, changed the variable name to name in string interpolation

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,7 @@
 user_number = int(input("Enter a number from 1 to 100: "))
 
-for i in range(user_number + 1):
-    if i == 0:
-        print(i)
-    elif (i % 3 == 0) and (i % 5 == 0):
+for i in range(1, user_number + 1):
+    if (i % 3 == 0) and (i % 5 == 0):
         print("fizzubzz")
     elif i % 3 == 0:
         print("fizz")
--- String_Interpolation.py
+++ String_Interpolation.py
@@ -1,5 +1,5 @@
-username = input("Enter your name: ")
+name = input("Enter your name: ")
 age = input("Enter your age: ")
 home = input("Enter where you live: ")
 
-print(f"My name is {username} and I am {age} years old. I live in {home}.")
+print(f"My name is {name} and I am {age} years old. I live in {home}.")
